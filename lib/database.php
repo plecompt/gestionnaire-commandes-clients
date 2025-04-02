e208a534983e041ba189cc51a84298d63a81a44d
Julien <hojulien02@gmail.com>
2025-04-02 13:42:01 +0200

    Initialisation de la classe Client et de la base de données PHP

---
+++ lib/database.php
@@ -0,0 +1,38 @@
+<?php 
+
+class DatabaseConnection {
+    
+    private ?\PDO $database = null;
+    
+    public function getConnection(): PDO
+    {
+        // prépare la connexion si elle n'existe pas
+        if ($this->database == null) {
+            $host = 'localhost';
+            $dbname = 'mvc-order';
+            $username = 'root';
+            $password = '';
+            $charset = 'utf8mb4';
+            
+            // dsn = data source name?
+            // crée une nouvelle connexion (pont) entre l'application php et la bd
+            $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+            
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ];
+            
+            // on essaye de créer un nouvel objet PDO avec les paramètres précédents pour initialiser le pont.
+            // si cela échoue, le programme quitte avec un message (getMessage() méthode de PDOException))
+            try {
+                $this->database = new PDO($dsn, $username, $password, $options);
+            } catch (PDOException $e) {
+                die('Erreur de connexion à la base de données : ' . $e->getMessage());
+            }
+        }
+        
+        return $this->database;
+    }
+    
+}
---
+++ models/Client.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once __DIR__ . '/../lib/database.php';
+
+class Client {
+    private int $idClient;
+    private string $nom;
+    private string $prenom;
+    private string $adresse;
+    private string $email;
+
+    public function getId(): int
+    {
+        return $this->idClient;
+    }
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    public function getAdresse(): string
+    {
+        return $this->adresse;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setNom(string $nom): void
+    {
+        $this->nom = htmlspecialchars($nom);
+    }
+
+    public function setPrenom(string $prenom): void
+    {
+        $this->prenom = htmlspecialchars($prenom);
+    }
+
+    public function setAdresse(string $adresse): void
+    {
+        $this->adresse = htmlspecialchars($adresse);
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+}
+
